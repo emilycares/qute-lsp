48c4375d12dac1d8fab2ebfaf123610ae19f6f38
micmine <micmine4@gmail.com>
2024-02-25 17:56:32 +0100

    format

--- plugins/jetbrains/src/main/kotlin/ch/micmine/qutelsp/QuteLspPreload.kt
+++ plugins/jetbrains/src/main/kotlin/ch/micmine/qutelsp/QuteLspPreload.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.application.PreloadingActivity
 import org.wso2.lsp4intellij.IntellijLanguageClient
 import org.wso2.lsp4intellij.client.languageserver.serverdefinition.RawCommandServerDefinition
 
-class QuteLspPreload: PreloadingActivity() {
+class QuteLspPreload : PreloadingActivity() {
     override suspend fun execute() {
         println(Language.getRegisteredLanguages())
         IntellijLanguageClient.addServerDefinition(RawCommandServerDefinition("html", arrayOf("qute-lsp")))
