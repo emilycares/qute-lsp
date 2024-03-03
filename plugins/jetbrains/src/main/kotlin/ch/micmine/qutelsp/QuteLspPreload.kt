63492600f23b5c33e95438fb96e2154ef8b079a5
micmine <micmine4@gmail.com>
2024-03-03 09:45:24 +0100

    remove unneded

--- plugins/jetbrains/src/main/kotlin/ch/micmine/qutelsp/QuteLspPreload.kt
+++ plugins/jetbrains/src/main/kotlin/ch/micmine/qutelsp/QuteLspPreload.kt
@@ -1,13 +1,11 @@
 package ch.micmine.qutelsp
 
-import com.intellij.lang.Language
 import com.intellij.openapi.application.PreloadingActivity
 import org.wso2.lsp4intellij.IntellijLanguageClient
 import org.wso2.lsp4intellij.client.languageserver.serverdefinition.RawCommandServerDefinition
 
 class QuteLspPreload : PreloadingActivity() {
     override suspend fun execute() {
-        println(Language.getRegisteredLanguages())
         IntellijLanguageClient.addServerDefinition(RawCommandServerDefinition("html", arrayOf("qute-lsp")))
     }
 }
